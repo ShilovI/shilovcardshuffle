baeef54eab10a1c0e5f2d66689747bb84ee479ec
Shilov <davidoff244@gmail.com>
2017-04-20 20:00:05 +0300

    Requires input until it complete. Fix mistakes.

--- src/main/java/Mixer.java
+++ src/main/java/Mixer.java
@@ -1,12 +1,10 @@
 import java.util.Arrays;
 
-/**
- * Created by Administrator on 4/20/2017.
- */
-public class Mixer {
+
+class Mixer {
     static int vi;
 
-    public static int[] create(int a) {
+    private static int[] create(int a) {
         int[] ints = new int[a];
         for (int x = 0; x < ints.length; x++) {
             ints[x] = x;
@@ -14,7 +12,7 @@ public class Mixer {
         return ints;
     }
 
-    public static int[] mix (int[] a) {
+    private static int[] mix (int[] a) {
         int[] mixedA = new int[a.length];
         for (int i = 0; i < mixedA.length/2; i = i+1) {
             mixedA[2*i] = a[i];
@@ -23,23 +21,8 @@ public class Mixer {
         return mixedA;
     }
 
-    /**public static void main(String[] args) {
-     Scanner in = new Scanner(System.in);
-     int get = in.nextInt();
-     int[] sample = Mixer.create(i);
-     int[] a = Mixer.mix(sample);
-     while (true) {
-     a = Mixer.mix(a);
-     counter++;
-     if (Arrays.equals(a, sample)) {
-     System.out.println("counter = " + counter);
-     break;
-     }
-     }
-     }
-     */
 
-    public static int counter(int i) {
+    static int counter(int i) {
         int[] sample = Mixer.create(i);
         int[] a = Mixer.mix(sample);
         int counter = 1;
--- src/main/java/MixerSetup.java
+++ src/main/java/MixerSetup.java
@@ -2,26 +2,27 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-/**
- * Created by Administrator on 4/20/2017.
- */
+
 public class MixerSetup {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int get = in.nextInt();
-        if (get%2 == 1) {
-            System.out.println("Incorrect value, value should be even");
-        } else {
-            for (int x = 2; x <= get; x = x + 2) {
-                try(FileWriter writer = new FileWriter("C:\\Users\\Davidoff244\\Desktop\\MixCounter.txt", true))
-                {
-                    String text = "(" + Integer.toString(x) + ";" + Integer.toString(Mixer.counter(x)) + ") ";
-                    writer.write(text);
-                }
-                catch(IOException ex){
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            int get = in.nextInt();
+            if (get%2 == 1) {
+                System.out.println("Incorrect value, value should be even");
+            } else {
+                for (int x = 2; x <= get; x = x + 2) {
+                    try(FileWriter writer = new FileWriter("C:\\Users\\Davidoff244\\Desktop\\MixCounter.txt", true))
+                    {
+                        String text = "(" + Integer.toString(x) + ";" + Integer.toString(Mixer.counter(x)) + ") ";
+                        writer.write(text);
+                    }
+                    catch(IOException ex){
 
-                    System.out.println(ex.getMessage());
+                        System.out.println(ex.getMessage());
+                    }
                 }
+                System.out.println("Success");
             }
         }
     }
